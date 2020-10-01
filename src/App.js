00c6a9b20052ce0c91f4f500bbaf726e3fdd0a5d
nikita <freenikita95@gmail.com>
2020-10-01 23:29:54 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+    BrowserRouter,
+    Switch,
+    Link
+} from "react-router-dom";
+import { Router } from './conteiner/routing/Routing'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Switch>
+                   <Router />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/routing/index.js
@@ -0,0 +1,20 @@
+import { HomePage } from "../pages/home";
+import { LoginPage } from "../pages/login";
+
+export const home = '/';
+export const login = '/login';
+
+export const router = [
+    {
+      page: HomePage,
+      path: home,
+        exact: true,
+        isPrivate: true
+    },
+    {
+      page: LoginPage,
+      path: login,
+        exact: false,
+        isPrivate: false
+    },
+]
