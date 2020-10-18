903f32d85571681670eb7efe3aee6bd154f9057c
nikita <freenikita95@gmail.com>
2020-10-19 02:12:19 +0300

    update privatRout

--- src/conteiners/wrapperPage/WrapperPage.js
+++ src/conteiners/wrapperPage/WrapperPage.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useUser } from 'reactfire';
 import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Container from '@material-ui/core/Container';
@@ -6,6 +7,9 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { Router } from 'conteiners/routing/Routing';
 import { AppBar } from 'components/AppBar';
 import { ToolBar } from 'components/ToolBar';
+import { useQuery } from '@apollo/client';
+import Cookies from 'js-cookie';
+import { Query } from 'API/graphql';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,6 +23,19 @@ const useStyles = makeStyles((theme) => ({
 
 export const WrapperPage = () => {
   const classes = useStyles();
+  const user = useUser();
+  const { uid } = user || {};
+
+  const { loading, error, data } = useQuery(Query.SET_USER, {
+    variables: { firebase_id: uid },
+    fetchPolicy: 'user',
+  });
+
+  useEffect(() => {
+    if (data) {
+      Cookies.set('user_id', data.setUser.id, { expires: 7 });
+    }
+  }, [data]);
 
   return (
     <div className={classes.root}>
